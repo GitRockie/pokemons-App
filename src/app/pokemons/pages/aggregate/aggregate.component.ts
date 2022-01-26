eae372715c68994183dca8feb56320165e9723ed
Roman Kostyuchenko <elromankos@gmail.com>
2022-01-26 15:03:36 +0100

    Basic app structure create
    Thinking in LazyLoad
    Auth Module create

---
+++ src/app/pokemons/pages/aggregate/aggregate.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aggregate',
+  templateUrl: './aggregate.component.html',
+  styles: [
+  ]
+})
+export class AggregateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
