8fe30fe0e3a7eaf2ec7f98798ce7b35cc410e1a6
Roman Kostyuchenko <elromankos@gmail.com>
2022-02-01 22:36:39 +0100

    Routes create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PokeDetailComponent } from './components/poke-detail/poke-detail.component';
+import { PokeTableComponent } from './components/poke-table/poke-table.component';
+
+const routes: Routes = [
+  { path: 'home', component: PokeTableComponent },
+  { path: 'pokemon/:id', component: PokeDetailComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '**', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
