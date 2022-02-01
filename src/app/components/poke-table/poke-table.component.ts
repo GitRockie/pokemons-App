7d90a9ec5c9b3c1789cd2b11da73380cfb500bd2
Roman Kostyuchenko <elromankos@gmail.com>
2022-02-02 00:51:12 +0100

    Table component work

--- src/app/components/poke-table/poke-table.component.ts
+++ src/app/components/poke-table/poke-table.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
@@ -9,6 +11,14 @@ import { PokemonService } from 'src/app/services/pokemon.service';
 })
 export class PokeTableComponent implements OnInit {
 
+  displayedColumns: string [] = ['position', 'image', 'pokename'];
+  data:any [] = [];
+  dataSource = new MatTableDataSource<any>(this.data);
+  pokemons = [];
+
+  @ViewChild(MatPaginator, { static: true })
+  paginator!: MatPaginator;
+
   constructor( private pokemonService: PokemonService ) { }
 
   ngOnInit(): void {
@@ -16,12 +26,34 @@ export class PokeTableComponent implements OnInit {
   }
 
   getPokemons() {
-    this.pokemonService.getPokemons()
+
+    let pokemonData:any;
+
+    for ( let i= 1; i <=150; i++ ) {
+    this.pokemonService.getPokemons( i )
         .subscribe(
           resp => {
-            console.log ( resp )
+            pokemonData = {
+              position: i,
+              image: resp.sprites.front_default,
+              pokename: resp.name
+            };
+            this.data.push( pokemonData );
+            this.dataSource = new MatTableDataSource<any>(this.data);
+            this.dataSource.paginator = this.paginator;
+            
           }
         );
+    }
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
 
