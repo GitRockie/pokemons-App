f3f36d87ab0d3b9a3024eec737856fe3e4214d67
Roman Kostyuchenko <elromankos@gmail.com>
2022-02-01 22:02:05 +0100

    Angular material install
    basic components create

---
+++ src/app/components/poke-detail/poke-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-poke-detail',
+  templateUrl: './poke-detail.component.html',
+  styles: [
+  ]
+})
+export class PokeDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/poke-table/poke-table.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-poke-table',
+  templateUrl: './poke-table.component.html',
+  styles: [
+  ]
+})
+export class PokeTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
